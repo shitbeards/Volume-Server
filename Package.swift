addebd66a34c0e7cd3a366b99cdf63dd2468e617
jamesstidard <jamesstidard@gmail.com>
2016-11-12 23:12:34 +0000

    get google books

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Server",
+    name: "Volume-Server",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
     ],
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -7,8 +7,15 @@ drop.get { req in
     	"message": drop.localization[req.lang, "welcome", "title"]
     ])
 }
-drop.get("hello") { request in
-    return "Hello, world!"
+
+drop.get("books") { request in
+    guard let query = request.data["query"]?.string else {
+        throw Abort.badRequest
+    }
+    return try drop.client.get("\(drop.config["google-books", "api-base-url"]!.string!)/volumes", query: [
+        "q": query,
+        "key": drop.config["google-books", "api-key"]!.string!
+    ])
 }
 
 drop.resource("posts", PostController())
